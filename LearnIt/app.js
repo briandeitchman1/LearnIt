765036d849c5c5d18417ff23e6466e78b85bfbc5
Brian Deitchman <briandeitchman@gmail.com>
2022-03-22 01:17:17 -0700

    Set ejs as the view engine in express.  /home /about routes are .ejs files now added about for testing

--- LearnIt/app.js
+++ LearnIt/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require("path")
 
+//lets us use ejs with express
+app.set('view engine', 'ejs')
 
+//serves files from the static path /view
+app.use('/views', express.static(path.join(__dirname, 'views')))
+
+const testData = "I sent this data from my JS file"
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.render('home', { testData })
 })
+app.get('/home', (req, res) => {
+    res.render('home', { testData })
+})
+app.get("/about", (req, res) => {
+    res.render('about')
+})
+
 
 
 app.listen(port, () => {
