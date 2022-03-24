863118629b56733429114e9a7ca9af75d358f76c
Brian Deitchman <briandeitchman@gmail.com>
2022-03-24 02:31:06 -0700

    updated the show page. fixed bug with the seeded data

--- LearnIt/seeds/index.js
+++ LearnIt/seeds/index.js
@@ -13,23 +13,36 @@ async function main() {
             console.log(err)
         })
     await mongoose.connect('mongodb://localhost:27017/test');
+    // const newStudyPage = new StudyPage({
+    //     flashCard: [{
+    //         term: "homeostasis",
+    //         definition: "any self-regulating process by which biological systems tend to maintain stability while adjusting to conditions that are optimal for survival."
+    //     }]
+    //     ,
+    //     multChoice: [{
+    //         question: "A molecule that increases the pH of a solution is called an acid.",
+    //         options: ["true", "false"],
+    //         answer: "false"
+    //     }]
+
+    // })
+    const newStudyPage = new StudyPage({
+    })
     for (let i = 0; i < 5; i++) {
-        const newStudyPage = new StudyPage({
-            flashCard: [{
-                term: "homeostasis",
-                definition: "any self-regulating process by which biological systems tend to maintain stability while adjusting to conditions that are optimal for survival."
-            }]
-            ,
-            multChoice: [{
+        newStudyPage.flashCard.push({
+            term: "homeostasis",
+            definition: "any self-regulating process by which biological systems tend to maintain stability while adjusting to conditions that are optimal for survival."
+        });
+
+        newStudyPage.multChoice.push(
+            {
                 question: "A molecule that increases the pH of a solution is called an acid.",
                 options: ["true", "false"],
                 answer: "false"
-            }]
-
-        })
-
-        await newStudyPage.save()
+            });
     }
+    await newStudyPage.save()
+
     console.log("new data inserted")
 }
 
